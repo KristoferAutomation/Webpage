d86655f1a7c4616372081d45cdeb4d39b988d3ee
KristoferWestergren <kristoferautomation@gmail.com>
2026-03-13 12:01:15 +0100

    optimized tha app

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
-import MyGlobals
-from streamlit_option_menu import option_menu
+import streamlit_option_menu
+
 
 # --- Streamlit config ---
-#layout="wide",
-st.set_page_config(page_title='Kristofer Automation AB', page_icon=MyGlobals.ICO)
+st.set_page_config(page_title='Kristofer Automation AB', page_icon='logo.ico')
 st.html("""<style>Mainmenu {visibility: hidden;}footer {visibility: hidden;}header {visibility: hidden;}""")
 
 
-st.image(MyGlobals.BANNER)
+st.image('banner.png')
 
 
-selected = option_menu(
+selected = streamlit_option_menu.option_menu(
 "Menu",
-["Home", "CV", "Contact", "This code"],
-icons=["house", "body-text", "envelope-at","code"],
+["Home", "CV", "Contact"],
+icons=["house", "body-text", "envelope-at"],
 menu_icon="cast",
 orientation="horizontal",
 default_index=0
@@ -22,12 +21,10 @@ default_index=0
 
 
 if selected == "CV":
-    st.image(MyGlobals.CV)
+    st.image('CV.png')
 elif selected == "Contact":
-    st.image(MyGlobals.CARD)
+    st.image('card.png')
     st.success('note new email: Kristofer@KristoferAutomation.se')
-elif selected == "This code":
-    st.image(MyGlobals.CODE)
 else:
-    st.image(MyGlobals.LOGO)
+    st.image('logo.png')
 
