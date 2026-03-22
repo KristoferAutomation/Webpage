ca3943f71015fbe7fa9201348b472c23bc3859ea
KristoferWestergren <kristoferautomation@gmail.com>
2026-03-22 10:43:39 +0100

    Prepared for pdf-handeler and moved images to ./assets

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import streamlit_option_menu
 
 
 # --- Streamlit config ---
-st.set_page_config(page_title='Kristofer Automation AB', page_icon='logo.ico')
+st.set_page_config(page_title='Kristofer Automation AB', page_icon='./assets/logo.ico')
 st.html("""<style>Mainmenu {visibility: hidden;}footer {visibility: hidden;}header {visibility: hidden;}""")
 
 
-st.image('banner.png')
+st.image('./assets/banner.png')
+
+
+# --- Header --
+st.header('Main page',text_alignment='center')
 
 
 selected = streamlit_option_menu.option_menu(
-    "Menu",
-    ["Home", "CV", "Contact"],
-    icons=["house", "body-text", "envelope-at"],
+    menu_title='',
+    options=["Home", "CV", "Contact"],#, "PDF-Handeler"],
+    icons=["house", "body-text", "envelope-at"],#, "filetype-pdf"],
     menu_icon="cast",
     orientation="horizontal",
     default_index=0
@@ -21,10 +25,12 @@ selected = streamlit_option_menu.option_menu(
 
 
 if selected == "CV":
-    st.image('CV.png')
+    st.image('./assets/CV.png')
 elif selected == "Contact":
-    st.image('card.png')
+    st.image('./assets/card.png')
     st.success('note new email: Kristofer@KristoferAutomation.se')
+elif selected == "PDF-Handeler":
+    st.switch_page('./pages/pdf_handeler.py')
 else:
-    st.image('logo.png')
+    st.image('./assets/logo.png')
 
